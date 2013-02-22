1b9b37cb0462fdfeafc16984da26fc94bce763fc
Stanislav Bitsko <Stanislav_Bitsko@epam.com>
2013-02-22 13:55:57 +0200

    Files cleanup

--- js/dnd.js
+++ js/dnd.js
@@ -6,7 +6,7 @@ function handleFileSelect(evt) {
     for (var i = 0, item; item = items[i]; i++) {
         //file = item.getAsFile();
         entry = item.webkitGetAsEntry();
-        traverseFileTree(entry, undefined, confFilter);
+        traverseFileTree2(entry);
     }
 }
 
@@ -15,28 +15,12 @@ function handleDragOver(evt) {
     evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
 }
 
-function confFilter(item) {
-    var glob = /^.*\.rb$/ // ruby files
-    var seek_vars = [".classpath", ".project"];
-    var filter_dirs = ["target", "src"];
-    if (filter_dirs.indexOf(item.name) != -1)
-        return false; // It's not on these dirs
-    if (seek_vars.indexOf(item.name) != -1)
-        return true; // Got one!
-    //if (glob.test(item.name))
-        //return true; // And another one!
-    if (item.isFile)
-        return false; // It's not on the hit list
-    return true; // recurse further
-}
-
-function traverseFileTree(entry, path, filter) {
+function traverseFileTree2(entry, path) {
     path = path || "";
-    filter = filter || function(entry) {return true;}
-
-    if (!filter(entry)) 
-        return;
     if (entry.isFile) {
+        filterFiles = [".classpath", ".project"];
+        if ($.inArray(entry.name, filterFiles) < 0)
+            return;
         // Get file
         entry.file(function(file) {
             parseContent(file, path);
@@ -47,10 +31,49 @@ function traverseFileTree(entry, path, filter) {
         // TODO: account for the fact that .readEntries is
         // not guaranteed to return all directory's entities
         dirReader.readEntries(function(entries) {
+            filterDirs = ["src", "target", ".git", ".settings", "reports", "env-install"];
+            entries = $.grep(entries, function(entry) { return $.inArray(entry.name, filterDirs) < 0; });
+
             for (var i=0; i<entries.length; i++) {
-                traverseFileTree(entries[i], path + entry.name + "/", filter);
+                traverseFileTree2(entries[i], path + entry.name + "/");
             }
         });
+    }
+}
+
+function handleFileChoose(evt) {
+    var files = evt.target.files; // FileList object
+
+    // Loop through the FileList and render image files as thumbnails.
+    for (var i = 0, f; f = files[i]; i++) {
+
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return function(e) {
+                // Render thumbnail.
+                var li = document.createElement('li');
+                $(li).text(e.target.result);
+                $("#parseList").before(li);
+                var xmlDoc = $.parseXML(e.target.result);
+                console.log(xmlDoc);
+                var xml = $("classpathentry", xmlDoc);
+                // all modules
+                var paths = []
+                $.each(xml, function(i, el) { 
+                    if(el.getAttribute("kind") === "var") 
+                    paths.push(el.getAttribute("path"))
+                });
+                //console.log(paths);
+                // our modules
+                var fpaths = paths.filter(function(el) { return el.indexOf("kf/atg/module") !== -1 })
+                console.log(fpaths);
+            };
+        })(f);
+
+        // Read in the image file as a data URL.
+        reader.readAsText(f);
     }
 }
 
@@ -61,8 +84,8 @@ function parseContent(file, path) {
     // Closure to capture the file information.
     reader.onload = (function(theFile) {
         return function(e) {
-            var xmlDoc = $(e.target.result.toString());
-            console.log(xmlDoc, theFile, path + theFile.name);
+            //var xmlDoc = $(e.target.result.toString());
+            //console.log(xmlDoc, theFile, path + theFile.name);
         };
     })(file);
 
@@ -70,6 +93,10 @@ function parseContent(file, path) {
 }
 
 // Setup the dnd listeners.
+//$("#drop_zone").bind({'dragover': handleDragOver, 'drop': handleFileSelect});
 var dropZone = document.getElementById('drop_zone');
 dropZone.addEventListener('dragover', handleDragOver, false);
 dropZone.addEventListener('drop', handleFileSelect, false);
+
+//$("#files").change(handleFileChoose);
+document.getElementById('files').addEventListener('change', handleFileChoose, false);
